843d4432a3e16a64234c4ad5054ac4061809db64
MrUltimate777 <bosaweluche@hotmail.com>
2024-03-21 21:26:07 +0000

    Removed self.width and self.height.

--- graphics.py
+++ graphics.py
@@ -2,8 +2,6 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title = "Maze Solver"
         self.canvas = Canvas(self.__root, bg="white", height=height, width=width)
