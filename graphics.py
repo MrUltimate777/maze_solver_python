c40b0b97c11a69d04bf9624af796743668bbd78b
MrUltimate777 <bosaweluche@hotmail.com>
2024-03-21 21:27:41 +0000

    Made all properties private.

--- graphics.py
+++ graphics.py
@@ -4,9 +4,9 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title = "Maze Solver"
-        self.canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
@@ -14,9 +14,9 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while (self.running):
+        self.__running = True
+        while (self.__running):
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
