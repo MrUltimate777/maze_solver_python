e75dac2b9374fa6bbc87f89e5c76a4fba4fe0867
MrUltimate777 <bosaweluche@hotmail.com>
2024-03-21 14:28:42 +0000

    Finished __init__.

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.root = Tk()
+        self.root.title = "Maze Solver"
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
