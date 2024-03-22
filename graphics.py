4c5d2665363fe560ace7f01c85e9e203a09f7e60
MrUltimate777 <bosaweluche@hotmail.com>
2024-03-22 14:02:08 +0000

    Created the draw method of Line.

--- graphics.py
+++ graphics.py
@@ -31,3 +31,9 @@ class Line:
     def __init__(self, point_1, point_2):
         self.start = point_1
         self.end = point_2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+        self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
+        )
+        canvas.pack()
