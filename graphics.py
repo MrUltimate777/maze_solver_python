4c2941dc72ed750a8371851e5be6e33ff58e64b5
MrUltimate777 <bosaweluche@hotmail.com>
2024-03-22 13:54:32 +0000

    Created the Line class and it's __init__.

--- graphics.py
+++ graphics.py
@@ -26,4 +26,8 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.start = point_1
+        self.end = point_2
